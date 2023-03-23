1c21998ca857989ea9764387fa880805b6cef4d5
Andrey Baev <53565925+mimoyo@users.noreply.github.com>
2023-03-23 13:31:07 +0200

    Feature/popup form (#4)
    * Created close popup button and form
    
    * Added styles to form and JS
    
    * Finished project

--- js/index.js
+++ js/index.js
@@ -1,10 +1,31 @@
 const body = document.querySelector('.body');
 const popup = body.querySelector('.popup');
 const popupOpenButton = body.querySelector('.profile__edit-button');
+const popupCloseButton = body.querySelector('.popup__close-button');
+const profileTitle = body.querySelector('.profile__title');
+const profileSubtitle = body.querySelector('.profile__subtitle');
+const formElement = body.querySelector('.form');
+const inputName = body.querySelector('.form__input_name');
+const inputJob = body.querySelector('.form__input_job');
 
 
 const openPopup = () => {
   popup.classList.add('popup_opened');
+  inputName.value = profileTitle.textContent;
+  inputJob.value = profileSubtitle.textContent;
+}
+
+const closePopup = () => {
+  popup.classList.remove('popup_opened');
+}
+
+const handleFormSubmit = (evt)  => {
+  evt.preventDefault();
+  profileTitle.textContent = inputName.value;
+  profileSubtitle.textContent = inputJob.value;
+  closePopup();
 }
 
 popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
