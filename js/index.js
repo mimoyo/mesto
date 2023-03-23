360cb9a4ede821e68508ddf419537c8bf36388c6
Andrey Baev <andrey@frostone.ru>
2023-03-23 10:42:18 +0200

    Created close popup button and form

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 const body = document.querySelector('.body');
 const popup = body.querySelector('.popup');
 const popupOpenButton = body.querySelector('.profile__edit-button');
+const popupCloseButton = body.querySelector('.popup__close-button');
 
 
 const openPopup = () => {
   popup.classList.add('popup_opened');
 }
 
+const closePopup = () => {
+  popup.classList.remove('popup_opened');
+}
+
 popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
