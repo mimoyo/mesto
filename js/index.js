1d61534e92aec57f35a590ce53cb83aa87d75ab8
Andrey Baev <andrey@frostone.ru>
2023-03-23 11:23:19 +0200

    Added styles to form and JS

--- js/index.js
+++ js/index.js
@@ -2,10 +2,16 @@ const body = document.querySelector('.body');
 const popup = body.querySelector('.popup');
 const popupOpenButton = body.querySelector('.profile__edit-button');
 const popupCloseButton = body.querySelector('.popup__close-button');
+const profileTitle = body.querySelector('.profile__title');
+const profileSubtitle = body.querySelector('.profile__subtitle');
+const inputName = body.querySelector('.form__input_name');
+const inputJob = body.querySelector('.form__input_job')
 
 
 const openPopup = () => {
   popup.classList.add('popup_opened');
+  inputName.value = profileTitle.textContent;
+  inputJob.value = profileSubtitle.textContent;
 }
 
 const closePopup = () => {
