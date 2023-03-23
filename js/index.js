db2bdad6b80ad91f2076dcf72a3a0470de8d78a4
Andrey Baev <andrey@frostone.ru>
2023-03-23 13:29:00 +0200

    Finished project

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@ const popupOpenButton = body.querySelector('.profile__edit-button');
 const popupCloseButton = body.querySelector('.popup__close-button');
 const profileTitle = body.querySelector('.profile__title');
 const profileSubtitle = body.querySelector('.profile__subtitle');
+const formElement = body.querySelector('.form');
 const inputName = body.querySelector('.form__input_name');
-const inputJob = body.querySelector('.form__input_job')
+const inputJob = body.querySelector('.form__input_job');
 
 
 const openPopup = () => {
@@ -18,5 +19,13 @@ const closePopup = () => {
   popup.classList.remove('popup_opened');
 }
 
+const handleFormSubmit = (evt)  => {
+  evt.preventDefault();
+  profileTitle.textContent = inputName.value;
+  profileSubtitle.textContent = inputJob.value;
+  closePopup();
+}
+
 popupOpenButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
